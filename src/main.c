dd4c9bfd4ba72f402a373b49eb75f47851d78e08
clibc <shirvani.abddullayev@gmail.com>
2021-10-13 23:55:26 +0400

    parsing tokens

--- src/main.c
+++ src/main.c
@@ -3,52 +3,27 @@
 
 #include "stack_member.h"
 #include "stack.hpp"
+#include "parser.hpp"
 
 int main(void)
 {
-    char str1[10]  = {"push"};
-    StackMember sm1 = {str1, INST, {PUSH}};
-    char str2[10]  = {"pop"};
-    StackMember sm2 = {str2, INST, {POP}};
-    char str3[10]  = {"sub"};
-    StackMember sm3 = {str3, INST, {SUB}};
-    
-    Stack s = create_stack();
-    push(&s, &sm1);
-    push(&s, &sm2);
-    push(&s, &sm3);
-    push(&s, &sm3);
-    StackMember* popped = pop(&s);
-    push(&s, &sm1);
+    char code[100] = {
+        "push 43\n"
+        "push 23\n"
+        "add\n"
+        "push 5\n"
+        "sub\n"
+    };
 
-    char parse_string[50] = "push 5 push 6 add return";
-    char* delim = " ";
+    Stack stack = create_stack();
 
-    StackMember stack[15];
+    char** tokens = 0;
+    int   token_count = 0;
+    parse_code(code, &tokens, &token_count);
 
-    char* parsed_tokens[30];
-
-    int first_iteration = 0;
-    int i = 0;
-    for(;;){
-        char* p;
-        if(!first_iteration){
-            p = strtok(parse_string, delim);
-            first_iteration = 1;
-        }
-        else { p = strtok(NULL, delim); }
-
-        if(p != NULL){
-            parsed_tokens[i++] = p;
-        }
-        else break;
+    for(int i = 0; i < token_count; ++i){
+        printf("%s\n", tokens[i]);
     }
 
-    for(int j = 0; j < i; ++j){
-        printf("%s\n", parsed_tokens[j]);
-    }
-    
-    (void)parsed_tokens;
-    (void)stack;
     return 0;
 }
---
+++ src/parser.hpp
@@ -0,0 +1,36 @@
+#ifndef PARSER
+#define PARSER
+
+#define MAX_LINES 20
+
+void parse_code(char* code, char*** tokens, int* token_count)
+{
+    char** tkns = malloc(sizeof(char*) * MAX_LINES);
+
+    char word_delim[] = " \t\r\n\v\f";
+    int t_count = 0;
+    int first_iteration = 1;
+    for(;;){
+        char* token;
+        if(first_iteration) {
+            token = strtok(code, word_delim);
+            first_iteration = 0;
+        }
+        else token = strtok(NULL, word_delim);
+
+        if(token == NULL) break; // end of tokens
+        
+        tkns[t_count] = token;
+        t_count += 1;
+
+        //printf("%s\n", tkns[t_count - 1]);
+    }
+
+    *tokens = tkns;
+    *token_count = t_count;
+}
+
+
+
+
+#endif//PARSER
