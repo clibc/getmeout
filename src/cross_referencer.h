93042d71c555eabae739aa498f0100c1a9dc2c07
clibc <shirvani.abddullayev@gmail.com>
2022-01-07 23:03:21 +0400

    new line string really fucked up but yeah

--- src/cross_referencer.h
+++ src/cross_referencer.h
@@ -8,6 +8,7 @@
 
 static void put_loop_vars_to_data_seg( int );
 static void put_strings_into_data_seg( Stack* );
+static int scan_string_newlines( char*, int );
 
 static void cross_ref( Stack* token_stack ) {
     Stack s       = create_stack( sizeof( StackMember ) );
@@ -24,7 +25,7 @@ static void cross_ref( Stack* token_stack ) {
         } else if ( m->i_type == ST_ELSE ) {
             StackMember* if_token = pop( &s );
             if ( if_token == NULL ) {
-                // TODO: Handle error
+                // @TODO: Handle error
                 printf( "if is missing" );
                 exit( -1 );
             }
@@ -35,7 +36,7 @@ static void cross_ref( Stack* token_stack ) {
         } else if ( m->i_type == ST_END ) {
             StackMember* token = pop( &s );
             if ( token == NULL ) {
-                // TODO: Handle error
+                // @TODO: Handle error
                 printf( "if/else is missing" );
                 exit( -1 );
             }
@@ -68,6 +69,22 @@ static void cross_ref( Stack* token_stack ) {
     put_strings_into_data_seg( token_stack );
 }
 
+/* static void put_strings_into_data_seg( Stack* stack ) { */
+/*     unsigned int string_id = 0; */
+
+/*     for ( int i = stack->item_count - 1; i > -1; --i ) { */
+/*         StackMember* member = (StackMember*)stack->base + i; */
+
+/*         if ( member->i_type == STRING ) { */
+/*             fput( "str%i db %s\n", string_id, member->string_value ); */
+/*             fput( "strLen%i equ $- str%i\n", string_id, string_id ); */
+/*             member->string_id_value = string_id; */
+
+/*             string_id += 1; */
+/*         } */
+/*     } */
+/* } */
+
 static void put_strings_into_data_seg( Stack* stack ) {
     unsigned int string_id = 0;
 
@@ -75,13 +92,53 @@ static void put_strings_into_data_seg( Stack* stack ) {
         StackMember* member = (StackMember*)stack->base + i;
 
         if ( member->i_type == STRING ) {
-            fput( "str%i db %s\n", string_id, member->string_value );
+            int newlines = scan_string_newlines( member->string_value, (int)strlen( member->string_value ) );
+            char* string = member->string_value;
+
+            if ( newlines == 0 ) {
+                fput( "str%i db %s\n", string_id, string );
+            } else {
+                fput( "str%i db ", string_id );
+                char* string      = member->string_value;
+                int string_lenght = strlen( string );
+                fput( "%s\",10,", string );
+                string += string_lenght + 2;  // one for n ( in "\n" )
+
+                for ( int j = 0; j < newlines; ++j ) {
+                    int string_lenght = strlen( string );
+                    if ( j == 0 ) {
+                        fput( "\"%s", string );
+                    } else
+                        fput( ",10,%s", string );
+                    string += string_lenght + 2;  // + 2 for \n
+                }
+                fput( "\n" );
+            }
             fput( "strLen%i equ $- str%i\n", string_id, string_id );
             member->string_id_value = string_id;
-
             string_id += 1;
         }
     }
+}
+
+static int scan_string_newlines( char* string, int size ) {
+    int counted_newlines = 0;
+    for ( int i = 0; i < size; ++i ) {
+        char letter = *( string + i );
+
+        if ( letter == '\\' ) {  // backslash detected search for n
+            char next_letter = *( string + i + 1 );
+            if ( next_letter == 'n' ) {  // match
+                *( string + i )     = 0;
+                *( string + i + 1 ) = 0;
+                counted_newlines += 1;
+            } else {
+                i += 1;
+            }
+        }
+    }
+
+    return counted_newlines;
 }
 
 static void put_loop_vars_to_data_seg( int total_loop_count ) {
